160075857c19ab488aa6b678449ba6a58be5ef1b
kirjh <71944013+kirjh@users.noreply.github.com>
2025-01-10 21:40:48 -0800

    Refactor variable names

--- Playership.cs
+++ Playership.cs
@@ -6,13 +6,15 @@ using System.Security;
 public partial class Playership : CharacterBody2D
 {
 	[Signal]
-	public delegate void CollidedEventHandler(float amount);
+	public delegate void CollidedEventHandler(float speed);
 	[Export]
-	public float MaxSpeed {get; set;} = 300f;
+	public float maxSpeed {get; set;} = 300f;
 	[Export]
-	public float Acceleration {get; set;} = 10f;
+	public float acceleration {get; set;} = 10f;
 	[Export]
-	public float RotationSpeed {get; set;} = 5f;
+	public float rotationSpeed {get; set;} = 5f;
+
+	private bool _allowCameraShake = true;
 
 	public override void _Ready() {
 		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play();
@@ -21,36 +23,36 @@ public partial class Playership : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		// Set Acceleration and Rotation respectively.
-		var newAcceleration = new Vector2(1,0) * Input.GetAxis("down", "up") * Acceleration;
-		var sideAcceleration = new Vector2(0,1) * Input.GetAxis("left", "right") * Acceleration/4;
+		var newAcceleration = new Vector2(1,0) * Input.GetAxis("down", "up") * acceleration;
+		var sideAcceleration = new Vector2(0,1) * Input.GetAxis("left", "right") * acceleration/4;
 		var targetAngle = Position.AngleToPoint(GetGlobalMousePosition()) - Rotation;
 
-		if (Mathf.Abs(targetAngle) < RotationSpeed * (float)delta) {
+		if (Mathf.Abs(targetAngle) < rotationSpeed * (float)delta) {
 			LookAt(GetGlobalMousePosition());
 		} else {
 			var _multiplier = 1;
 			if ((targetAngle > 0 && targetAngle > Math.PI) || (targetAngle < 0 && targetAngle > -1 * Math.PI)) {
 				_multiplier = -1;
 			}
-			Rotation += _multiplier * RotationSpeed * (float)delta;
+			Rotation += _multiplier * rotationSpeed * (float)delta;
 		}	
 
 		newAcceleration = newAcceleration.Rotated(Rotation);
 		sideAcceleration = sideAcceleration.Rotated(Rotation);
 
-		// Constrain Velocity to MaxSpeed.
-		Velocity = (Velocity + newAcceleration + sideAcceleration).LimitLength(MaxSpeed);
+		// Constrain Velocity to maxSpeed.
+		Velocity = (Velocity + newAcceleration + sideAcceleration).LimitLength(maxSpeed);
 
 		// Disable Particles if not moving.
 		var Particles = GetNode<CpuParticles2D>("CPUParticles2D");
 		//Particles.InitialVelocityMin = Velocity.Length() / 7;
 		//Particles.InitialVelocityMax = Velocity.Length() / 5;
-		Particles.Emitting = (Velocity.Length() < Acceleration) ? false : true;
+		Particles.Emitting = (Velocity.Length() < acceleration) ? false : true;
 
 		// Process Decceleration.
 		if (Input.IsActionPressed("stop")) {
 			if (!Velocity.IsZeroApprox()) {
-				Velocity += -1 * Velocity.LimitLength(Acceleration);;
+				Velocity += -1 * Velocity.LimitLength(acceleration);;
 			} else {
 				Velocity = Vector2.Zero;
 			}
@@ -58,7 +60,13 @@ public partial class Playership : CharacterBody2D
 		//GD.Print(Rotation + " | " + Velocity.X + ", " + Velocity.Y);
 		var collision = MoveAndCollide(Velocity * (float)delta);
 		if (collision != null) {
-			EmitSignal(SignalName.Collided, 1f);
+			// Emit Collision signal
+			EmitSignal(SignalName.Collided, Velocity.Length() / maxSpeed);
+			_allowCameraShake = false;
+			var timer = GetNode<Timer>("CollisionTimer");
+			timer.Start();
+
+			// Bounce off of collided object
 			Velocity = Velocity.Bounce(collision.GetNormal()) / 2;
 		}
 	}
